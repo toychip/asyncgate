3370aaff30208b4ee6e4912eee15ef3177c14dd9
toychip <dlawnsgud427@naver.com>
2025-01-25 13:29:28 +0900

    [BE] feat: discovery-server 추가 (#1)

---
+++ src/backend/discovery-server/src/test/java/com/asyncgate/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asyncgate.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
