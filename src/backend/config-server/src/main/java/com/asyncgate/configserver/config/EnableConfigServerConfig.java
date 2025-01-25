eef0443a6a880afdb4cf80ac5736591312f4b516
toychip <dlawnsgud427@naver.com>
2025-01-25 12:08:48 +0900

    [BE] feat: Config Server 추가 (#1)

---
+++ src/backend/config-server/src/main/java/com/asyncgate/configserver/config/EnableConfigServerConfig.java
@@ -0,0 +1,9 @@
+package com.asyncgate.configserver.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+public class EnableConfigServerConfig {
+}
