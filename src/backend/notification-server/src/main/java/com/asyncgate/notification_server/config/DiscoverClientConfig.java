552e56cd81407556f41d574c02793858506b58db
toychip <dlawnsgud427@naver.com>
2025-01-27 15:48:30 +0900

    [BE] feat: notification-server init (#3)

---
+++ src/backend/notification-server/src/main/java/com/asyncgate/notification_server/config/DiscoverClientConfig.java
@@ -0,0 +1,9 @@
+package com.asyncgate.notification_server.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class DiscoverClientConfig {
+}
---
+++ src/backend/notification-server/src/test/java/com/asyncgate/notification_server/NotificationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asyncgate.notification_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
