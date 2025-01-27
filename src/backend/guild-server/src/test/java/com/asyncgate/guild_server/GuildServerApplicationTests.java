d60fe40b01425e9bd6da774437f650a35f5ad580
toychip <dlawnsgud427@naver.com>
2025-01-27 15:58:26 +0900

    [BE] feat: guild-server init (#3)

---
+++ src/backend/guild-server/src/test/java/com/asyncgate/guild_server/GuildServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asyncgate.guild_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuildServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
