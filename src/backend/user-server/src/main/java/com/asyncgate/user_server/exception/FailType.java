1281ae23ff0a021f7eb6292f5a7c232c54384717
EunJiJung <bianbbc87@gmail.com>
2025-01-29 03:09:47 +0900

    [BE] refactor : 500 에러 접미사 추가(#5)
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/backend/user-server/src/main/java/com/asyncgate/user_server/exception/FailType.java
+++ src/backend/user-server/src/main/java/com/asyncgate/user_server/exception/FailType.java
@@ -11,8 +11,8 @@ public enum FailType {
     JWT_BAD_REQUEST(HttpStatus.BAD_REQUEST, "Jwt_4001", "잘못된 JWT Token 타입입니다."),
 
     // S3
-    UPLOAD_FILE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S3_5001", "S3 이미지 업로드 실패"),
-    DELETE_FILE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S3_5002", "S3 이미지 제거 실패");
+    _UPLOAD_FILE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S3_5001", "S3 이미지 업로드 실패"),
+    _DELETE_FILE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S3_5002", "S3 이미지 제거 실패");
 
     ;
     private final HttpStatus status;
