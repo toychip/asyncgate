d4080a1f9cd9d95d9135f0aa3a6c677a7aefe848
toychip <dlawnsgud427@naver.com>
2025-01-31 02:34:56 +0900

    [BE] feat: BaseEntity, BaseTimeEntity 추가 (#9)

---
+++ src/backend/guild-server/src/main/java/com/asyncgate/guild_server/config/BaseEntityConfig.java
@@ -0,0 +1,9 @@
+package com.asyncgate.guild_server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class BaseEntityConfig {
+}
