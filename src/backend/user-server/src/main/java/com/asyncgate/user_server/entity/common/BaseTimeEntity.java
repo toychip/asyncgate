07dbe181f457c113dd2b03dd54c2998f8279cce9
EunJiJung <bianbbc87@gmail.com>
2025-02-01 23:47:33 +0900

    [BE] refactor: base entity, time entity 분리 (#10)
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/backend/user-server/src/main/java/com/asyncgate/user_server/entity/common/BaseTimeEntity.java
+++ src/backend/user-server/src/main/java/com/asyncgate/user_server/entity/common/BaseTimeEntity.java
@@ -4,9 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -15,24 +13,13 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity extends BaseTimeEntity {
+public abstract class BaseTimeEntity {
 
-    @CreatedBy
+    @CreatedDate
     @Column(updatable = false)
-    private String createdBy;
+    private LocalDateTime createdDate;
 
-    @LastModifiedBy
-    private String lastModifiedBy;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 
-    private boolean deleted;
-
-    // 재활성화 - soft delete
-    public void activate() {
-        this.deleted = false;
-    }
-
-    // 비활성화 - soft delete
-    public void deactivate() {
-        this.deleted = true;
-    }
 }
