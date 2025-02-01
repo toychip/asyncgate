54c363b0c9890dec84b8c861c3b0f3c101b971b1
EunJiJung <bianbbc87@gmail.com>
2025-02-01 19:27:26 +0900

    [BE] refactor: user details account non expired, account non locked, credentials non expried, is enabled 오버라이딩 제거 (#10)
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/backend/user-server/src/main/java/com/asyncgate/user_server/security/info/CustomUserPrincipal.java
+++ src/backend/user-server/src/main/java/com/asyncgate/user_server/security/info/CustomUserPrincipal.java
@@ -47,22 +47,18 @@ public class CustomUserPrincipal implements UserDetails {
         return password;
     }
 
-    @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
-    @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
-    @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
-    @Override
     public boolean isEnabled() {
         return true;
     }
