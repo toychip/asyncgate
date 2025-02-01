68d81b526faa33185478f1a04f927de8a79fce5e
EunJiJung <bianbbc87@gmail.com>
2025-02-01 18:48:23 +0900

    [BE] refactor: 비밀번호 중복 검사 BCryptPasswordEncoder로 변경 (#10)
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/backend/user-server/src/main/java/com/asyncgate/user_server/service/LoginMemberService.java
+++ src/backend/user-server/src/main/java/com/asyncgate/user_server/service/LoginMemberService.java
@@ -9,22 +9,25 @@ import com.asyncgate.user_server.repository.MemberRepository;
 import com.asyncgate.user_server.security.utility.JsonWebTokenUtil;
 import com.asyncgate.user_server.usecase.LoginMemberUsecase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class LoginMemberService implements LoginMemberUsecase {
     private final MemberRepository memberRepository;
     private final JsonWebTokenUtil jsonWebTokenUtil;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
+    @Transactional(readOnly = true)
     public DefaultJsonWebTokenResponse execute(final LoginMemberRequest request) {
         Member member = memberRepository.findByEmail(request.email())
                 .orElseThrow(() -> new UserServerException(FailType.MEMBER_NOT_EXIST_EMAIL));
-        if (member == null) {
-            throw new UserServerException(FailType.MEMBER_NOT_EXIST_EMAIL);
-        }
-        if (!member.getPassword().equals(request.password())) {
+
+        // 비밀번호 일치 인증
+        if (!bCryptPasswordEncoder.matches(request.password(), member.getPassword())) {
             throw new UserServerException(FailType._INVALID_PASSWORD);
         }
 
