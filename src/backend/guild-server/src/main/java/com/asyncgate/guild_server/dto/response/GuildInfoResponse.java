accd849e0d2f73c2bddca94e49d347ac436429cd
toychip <dlawnsgud427@naver.com>
2025-02-01 11:42:38 +0900

    [BE] refactor: 반환값 내부 category, channel에서 중복 guildId, channelId 제거 (#9)

--- src/backend/guild-server/src/main/java/com/asyncgate/guild_server/dto/response/GuildInfoResponse.java
+++ src/backend/guild-server/src/main/java/com/asyncgate/guild_server/dto/response/GuildInfoResponse.java
@@ -1,12 +1,42 @@
 package com.asyncgate.guild_server.dto.response;
 
-
+import com.asyncgate.guild_server.domain.ChannelType;
 import java.util.List;
 
-public record GuildInfoResponse(GuildResponse guild, List<CategoryResponse> categories, List<ChannelResponse> channels) {
+public record GuildInfoResponse(
+        GuildResponse guild,
+        List<InnerCategoryResponse> categories,
+        List<InnerChannelResponse> channels
+) {
 
-    public static GuildInfoResponse of(GuildResponse guild, List<CategoryResponse> categories,
-                                       List<ChannelResponse> channels) {
-        return new GuildInfoResponse(guild, categories, channels);
+    public static GuildInfoResponse of(
+            final GuildResponse guild,
+            final List<CategoryResponse> categories,
+            final List<ChannelResponse> channels
+    ) {
+        List<InnerCategoryResponse> innerCategories = transInnerCategory(categories);
+        List<InnerChannelResponse> innerChannels = transInnerChannels(channels);
+        return new GuildInfoResponse(guild, innerCategories, innerChannels);
+    }
+
+    private static List<InnerChannelResponse> transInnerChannels(List<ChannelResponse> channels) {
+        return channels.stream().map(
+                channel -> new InnerChannelResponse(channel.channelId(), channel.name(), channel.topic(), channel.channelType(), channel.isPrivate())
+        ).toList();
+    }
+
+    private static List<InnerCategoryResponse> transInnerCategory(List<CategoryResponse> categories) {
+        return categories.stream().map(
+                category -> new InnerCategoryResponse(category.categoryId(), category.name(), category.isPrivate())
+        ).toList();
+    }
+
+    private record InnerCategoryResponse(String categoryId, String name, boolean isPrivate) {
+    }
+
+    private record InnerChannelResponse(
+            String channelId, String name, String topic,
+            ChannelType channelType, boolean isPrivate
+    ) {
     }
 }
