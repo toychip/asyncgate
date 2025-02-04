af8034cd7f109800068feff30db565ea84540334
toychip <dlawnsgud427@naver.com>
2025-02-04 23:06:36 +0900

    [BE] feat: WebSocketConfig 추가 (#22)

--- src/backend/chat-server/build.gradle.kts
+++ src/backend/chat-server/build.gradle.kts
@@ -40,15 +40,22 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
+    implementation ("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    	// AWS
+    // AWS
 	implementation ("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
+
+    implementation ("org.springframework.boot:spring-boot-starter-websocket")
+
+    implementation ("org.springframework.kafka:spring-kafka")
+    
+    implementation ("org.springframework.boot:spring-boot-starter-data-mongodb")
 }
 
 dependencyManagement {
