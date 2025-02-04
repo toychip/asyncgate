56b44dc9141bb605ac031bf9d7746c5b41103fe1
KDK <one_good07@naver.com>
2025-02-04 14:17:32 +0900

    [iOS] feat: iOS 프로젝트 생성

---
+++ src/ios/asyncgate_iOS/asyncgate_iOS/asyncgate_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  asyncgate_iOSApp.swift
+//  asyncgate_iOS
+//
+//  Created by kdk on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct asyncgate_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
