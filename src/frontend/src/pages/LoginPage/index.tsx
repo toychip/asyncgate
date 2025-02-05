a1f4b6dfc7dc84c07d8bcafbcceb4e1742b631db
chysis <chysiss@naver.com>
2025-02-06 02:31:53 +0900

    [FE] design: 로그인 페이지 UI 구현 (#29)

--- src/frontend/src/pages/LoginPage/index.tsx
+++ src/frontend/src/pages/LoginPage/index.tsx
@@ -1,5 +1,51 @@
+import LogoIcon from '@/assets/logo.svg';
+import Input from '@/components/common/Input';
+
+import * as S from './styles';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <>
+      <S.PageContainer>
+        <S.Background />
+        <S.Logo>
+          <img src={LogoIcon} alt=""></img>
+        </S.Logo>
+        <S.ContentWrapper>
+          <S.LoginFormContainer>
+            <S.MainLoginContainer>
+              <S.LoginFormHeader>
+                <S.HeaderTitle>돌아오신 것을 환영해요!</S.HeaderTitle>
+                <S.HeaderSubtitle>다시 만나다니 너무 반가워요!</S.HeaderSubtitle>
+              </S.LoginFormHeader>
+              <S.InputContainer>
+                <Input label="이메일 또는 전화번호" isRequired={true} />
+                <Input label="비밀번호" type="password" isRequired={true} />
+              </S.InputContainer>
+              <S.ForgotPasswordButton>비밀번호를 잊으셨나요?</S.ForgotPasswordButton>
+              <S.LoginButton>
+                <S.LoginText>로그인</S.LoginText>
+              </S.LoginButton>
+              <S.ToggleRegisterContainer>
+                <S.RegisterText>계정이 필요한가요?</S.RegisterText>
+                <S.RegisterButton>가입하기</S.RegisterButton>
+              </S.ToggleRegisterContainer>
+            </S.MainLoginContainer>
+            <S.QRCodeLoginContainer>
+              <S.QRCodeWrapper>
+                <S.QRCodeImage />
+              </S.QRCodeWrapper>
+              <S.QRCodeLoginTitle>QR 코드로 로그인</S.QRCodeLoginTitle>
+              <S.QRCodeLoginLabel>
+                <strong>AsyncGate 모바일 앱</strong>으로 스캔해 바로 로그인하세요.
+              </S.QRCodeLoginLabel>
+              <S.PassKeyLoginButton>또는, 패스키로 로그인하세요</S.PassKeyLoginButton>
+            </S.QRCodeLoginContainer>
+          </S.LoginFormContainer>
+        </S.ContentWrapper>
+      </S.PageContainer>
+    </>
+  );
 };
 
 export default LoginPage;
---
+++ src/frontend/src/pages/LoginPage/styles.ts
@@ -0,0 +1,173 @@
+import styled from 'styled-components';
+
+import {
+  BodyMediumText,
+  BodyRegularText,
+  CaptionText,
+  HeaderText,
+  MediumButtonText,
+  TitleText1,
+} from '@/styles/Typography';
+
+export const Background = styled.div`
+  position: fixed;
+  z-index: 0;
+  top: 0;
+  left: 0;
+
+  width: 100vw;
+  height: 100vh;
+
+  background-image: url('/src/assets/background.svg');
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+export const PageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Logo = styled.div`
+  position: absolute;
+  top: 4.8rem;
+  left: 4.8rem;
+`;
+
+export const ContentWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const LoginFormContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  width: 78.4rem;
+  height: 40.8rem;
+  padding: 3.2rem;
+  border-radius: 0.5rem;
+
+  background-color: ${({ theme }) => theme.colors.dark[600]};
+`;
+
+export const MainLoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 41.6rem;
+  height: 34.4rem;
+`;
+
+export const LoginFormHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled(HeaderText)`
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const HeaderSubtitle = styled(BodyMediumText)`
+  color: ${({ theme }) => theme.colors.dark[300]};
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  margin-top: 2rem;
+`;
+
+export const ForgotPasswordButton = styled.button`
+  display: flex;
+  justify-content: flex-start;
+  margin: 0.4rem 0 2rem;
+  color: ${({ theme }) => theme.colors.link};
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+export const LoginButton = styled.button`
+  width: 100%;
+  height: 4.4rem;
+  border-radius: 0.3rem;
+
+  color: ${({ theme }) => theme.colors.white};
+
+  background-color: ${({ theme }) => theme.colors.blue};
+`;
+
+export const LoginText = styled(MediumButtonText)`
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const ToggleRegisterContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 0.4rem;
+`;
+
+export const RegisterText = styled(CaptionText)`
+  color: ${({ theme }) => theme.colors.dark[300]};
+`;
+
+export const RegisterButton = styled.button`
+  margin-left: 0.4rem;
+  color: ${({ theme }) => theme.colors.link};
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+export const QRCodeLoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 24rem;
+  height: 34.4rem;
+`;
+
+export const QRCodeWrapper = styled.div`
+  overflow: hidden;
+
+  width: 17.6rem;
+  height: 17.6rem;
+  margin-bottom: 3.2rem;
+  border-radius: 0.4rem;
+
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+export const QRCodeImage = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: url('/src/assets/qrcode.svg');
+  background-size: cover;
+`;
+
+export const QRCodeLoginTitle = styled(TitleText1)`
+  margin-bottom: 0.8rem;
+  font-size: 2.4rem;
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const QRCodeLoginLabel = styled(BodyRegularText)`
+  line-height: 1.25;
+  color: ${({ theme }) => theme.colors.dark[300]};
+  text-align: center;
+`;
+
+export const PassKeyLoginButton = styled.button`
+  height: 4.4rem;
+  color: ${({ theme }) => theme.colors.link};
+`;
