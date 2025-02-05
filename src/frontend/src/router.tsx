227dcf62be663dce29b3d1726e2c7761c287693f
Fe <chysiss@naver.com>
2025-02-05 17:52:33 +0900

    [FE] refactor: 라우터 구조 변경 (#29)

--- src/frontend/src/router.tsx
+++ src/frontend/src/router.tsx
@@ -1,26 +1,38 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import App from './App';
-import ErrorPage from './pages/ErrorPage';
+import AuthFullLayout from './components/layout/AuthFullLayout';
+import FullLayout from './components/layout/FullLayout';
+import PublicOnlyLayout from './components/layout/PublicOnlyLayout';
+import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
+    element: <FullLayout/>,
     children: [
       {
-        path: 'login',
-        element: <LoginPage />,
+        path: '/', element: <LandingPage />
+      }
+    ]
+  },
+  {
+    element: <PublicOnlyLayout/>,
+    children: [
+      {
+        path: '/login', element: <LoginPage/>
       },
       {
-        path: 'register',
-        element: <RegisterPage />,
-      },
-    ],
+        path: '/register', element: <RegisterPage/>
+      }
+    ]
   },
+  {
+    element: <AuthFullLayout/>,
+    children: [
+      
+    ]
+  }
 ]);
 
 export default router;
