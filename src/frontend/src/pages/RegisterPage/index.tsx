ec73d76b433e44b7d4f5b83e066c2a476c01f45c
chysis <chysiss@naver.com>
2025-02-09 22:34:44 +0900

    [FE] feat: 회원가입 페이지 구현 (#29)

--- src/frontend/src/pages/RegisterPage/index.tsx
+++ src/frontend/src/pages/RegisterPage/index.tsx
@@ -1,5 +1,46 @@
+import AuthCheckbox from '@/components/common/AuthCheckbox';
+import AuthDateInput from '@/components/common/AuthDateInput';
+import AuthInput from '@/components/common/AuthInput';
+
+import * as S from './styles';
+
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <S.PageContainer>
+      <S.ContentWrapper>
+        <S.RegisterFormContainer>
+          <S.FormTitle>계정 만들기</S.FormTitle>
+          <S.FormBody>
+            <S.InputContainer>
+              <AuthInput id="email" label="이메일" isRequired={true} />
+              <AuthInput id="nickname" label="별명" />
+              <AuthInput id="username" label="사용자명" isRequired={true} />
+              <AuthInput id="password" label="비밀번호" type="password" isRequired={true} />
+              <AuthDateInput id="birthdate" label="생년월일" isRequired={true} />
+            </S.InputContainer>
+            <S.EmailOptInContainer>
+              <AuthCheckbox
+                id="optIn"
+                isChecked={true}
+                label="(선택사항) AsyncGate 소식, 도움말, 특별 할인을 이메일로 보내주세요.
+              언제든지 취소하실 수 있어요."
+              />
+            </S.EmailOptInContainer>
+            <S.ContinueButton>
+              <S.ContinueText>계속하기</S.ContinueText>
+            </S.ContinueButton>
+            <S.PrivacyPolicyLabel>
+              등록하는 순간 AsyncGate의 <S.LinkButton>서비스 이용 약관</S.LinkButton>과
+              <S.LinkButton>개인정보 보호 정책</S.LinkButton>에 동의하게 됩니다.
+            </S.PrivacyPolicyLabel>
+            <S.ToggleLoginButton>
+              <S.LoginText>이미 계정이 있으신가요?</S.LoginText>
+            </S.ToggleLoginButton>
+          </S.FormBody>
+        </S.RegisterFormContainer>
+      </S.ContentWrapper>
+    </S.PageContainer>
+  );
 };
 
 export default RegisterPage;
---
+++ src/frontend/src/pages/RegisterPage/styles.ts
@@ -0,0 +1,101 @@
+import styled from 'styled-components';
+
+import { BodyMediumText, ChipText, HeaderText, SmallText } from '@/styles/Typography';
+
+export const PageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const ContentWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const RegisterFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 48rem;
+  height: 69.8rem;
+  padding: 3.2rem;
+  border-radius: 0.5rem;
+
+  background-color: ${({ theme }) => theme.colors.dark[600]};
+`;
+
+export const FormTitle = styled(HeaderText)`
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const FormBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  width: 100%;
+  margin-top: 2rem;
+`;
+
+export const EmailOptInContainer = styled.div`
+  display: flex;
+  margin-top: 0.8rem;
+`;
+
+export const EmailOptInLabel = styled(SmallText)`
+  color: ${({ theme }) => theme.colors.dark[300]};
+`;
+
+export const ContinueButton = styled.button`
+  width: 100%;
+  height: 4.4rem;
+  margin-top: 2rem;
+  border-radius: 0.3rem;
+
+  background-color: ${({ theme }) => theme.colors.blue};
+`;
+
+export const ContinueText = styled(BodyMediumText)`
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const PrivacyPolicyLabel = styled(SmallText)`
+  margin-top: 0.8rem;
+  line-height: 1.6rem;
+  color: ${({ theme }) => theme.colors.dark[300]};
+`;
+
+export const LinkButton = styled.button`
+  width: fit-content;
+  height: 1.6rem;
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.colors.link};
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const ToggleLoginButton = styled.button`
+  display: flex;
+  margin-top: 2rem;
+`;
+
+export const LoginText = styled(ChipText)`
+  height: 1.8rem;
+  color: ${({ theme }) => theme.colors.link};
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
