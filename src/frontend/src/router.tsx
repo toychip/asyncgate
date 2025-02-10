4664d18fbf93b6032bd9a3b3ef72d9724a37f9e9
zelkovaria <hyein396@gmail.com>
2025-02-10 12:05:13 +0900

    [FE] feat: router 설정 (#31)

--- src/frontend/src/router.tsx
+++ src/frontend/src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Outlet } from 'react-router-dom';
 
 import AuthFullLayout from './components/layout/AuthFullLayout';
 import FullLayout from './components/layout/FullLayout';
@@ -6,33 +6,50 @@ import PublicOnlyLayout from './components/layout/PublicOnlyLayout';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ModalRenderer from './components/common/ModalRender';
+import FriendsPage from './pages/FriendsPage';
 
 const router = createBrowserRouter([
   {
-    element: <FullLayout />,
+    element: (
+      <>
+        <Outlet />
+        <ModalRenderer />
+      </>
+    ),
     children: [
       {
-        path: '/',
-        element: <LandingPage />,
-      },
-    ],
-  },
-  {
-    element: <PublicOnlyLayout />,
-    children: [
-      {
-        path: '/login',
-        element: <LoginPage />,
+        element: <FullLayout />,
+        children: [
+          {
+            path: '/',
+            element: <LandingPage />,
+          },
+        ],
       },
       {
-        path: '/register',
-        element: <RegisterPage />,
+        element: <PublicOnlyLayout />,
+        children: [
+          {
+            path: '/login',
+            element: <LoginPage />,
+          },
+          {
+            path: '/register',
+            element: <RegisterPage />,
+          },
+        ],
+      },
+      {
+        element: <AuthFullLayout />,
+        children: [
+          {
+            path: '/friends',
+            element: <FriendsPage />,
+          },
+        ],
       },
     ],
-  },
-  {
-    element: <AuthFullLayout />,
-    children: [],
   },
 ]);
 
