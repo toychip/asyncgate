bedc52b8bff029e7534c488b0d4f871a70be2625
zelkovaria <hyein396@gmail.com>
2025-02-10 14:31:41 +0900

    [FE] feat: 메인 Friends 페이지 추가 (#31)

---
+++ src/frontend/src/pages/FriendsPage/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const FriendsPage = styled.div`
+  width: 100%;
+  height: 72rem;
+  background-color: ${({ theme }) => theme.colors.dark[500]};
+`;
