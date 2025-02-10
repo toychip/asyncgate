ea7eaf83670ec2c1a4f4d9fb7d2328ed2c6015f6
zelkovaria <hyein396@gmail.com>
2025-02-10 14:24:37 +0900

    [FE] feat: 길드 생성 버튼 클릭시 모달 동작 추가 (#31)

--- src/frontend/src/components/guild/GuildList/index.tsx
+++ src/frontend/src/components/guild/GuildList/index.tsx
@@ -1,13 +1,22 @@
+import CreateGuildModal from '../../../pages/FriendsPage/components/CreateGuildModal';
+import useModalStore from '../../../stores/modalStore';
+
 import * as S from './styles';
 
 const GuildList = () => {
+  const { openModal } = useModalStore();
+
+  const handleOpenModal = () => {
+    openModal('basic', 'create-server', <CreateGuildModal />);
+  };
+
   return (
     <S.GuildList>
       <S.DMButton>
         <S.DiscordIcon size={32} />
       </S.DMButton>
       {/* 서버 리스트 추가 예정 */}
-      <S.AddServerButton>
+      <S.AddServerButton onClick={handleOpenModal}>
         <S.PlusIcon size={24} />
       </S.AddServerButton>
       <S.SearchCommunityButton>
