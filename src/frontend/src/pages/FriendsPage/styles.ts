cdd15b96251c4e347911a68dbeaf76fde0459266
zelkovaria <hyein396@gmail.com>
2025-02-11 18:53:58 +0900

    [FE] refactor: 페이지 화면 비율 수정 (#31)

--- src/frontend/src/pages/FriendsPage/styles.ts
+++ src/frontend/src/pages/FriendsPage/styles.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const FriendsPage = styled.div`
   width: 100%;
-  height: 72rem;
+  height: 100vh;
   background-color: ${({ theme }) => theme.colors.dark[500]};
 `;
