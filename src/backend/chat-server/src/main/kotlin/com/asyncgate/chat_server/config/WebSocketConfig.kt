48580c2d4a0af1729dfe769566cbf223981da3d5
toychip <dlawnsgud427@naver.com>
2025-02-13 21:08:00 +0900

    [BE] feat: WebSocket SimpleBroker 등록 및 사용 (#22)

--- src/backend/chat-server/src/main/kotlin/com/asyncgate/chat_server/config/WebSocketConfig.kt
+++ src/backend/chat-server/src/main/kotlin/com/asyncgate/chat_server/config/WebSocketConfig.kt
@@ -24,6 +24,7 @@ class WebSocketConfig(
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
         registry.setApplicationDestinationPrefixes("/kafka")
+        registry.enableSimpleBroker("/topic/")
     }
 
     override fun configureClientInboundChannel(registration: ChannelRegistration) {
