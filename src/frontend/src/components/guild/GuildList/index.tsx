6bc1edba1ac39e9aec7631df69b593cc2bc0eaeb
zelkovaria <hyein396@gmail.com>
2025-02-14 17:43:16 +0900

    [FE] refactor: 길드 생성 모달에 퍼널 구조 적용 (#31)

--- src/frontend/src/components/guild/GuildList/index.tsx
+++ src/frontend/src/components/guild/GuildList/index.tsx
@@ -1,48 +1,15 @@
-import { useEffect, useState } from 'react';
-
-import CreateGuildModal from '../../../pages/FriendsPage/components/CreateGuildModal';
-import CustomizeGuildModal from '../../../pages/FriendsPage/components/CustomizeGuildModal';
-import useModalStore from '../../../stores/modalStore';
-import { CreateGuildStep } from '../types';
+import AddGuildButton from '../AddGuildButton';
 
 import * as S from './styles';
 
 const GuildList = () => {
-  const { openModal, modal } = useModalStore();
-  const [currentModal, setCurrentModal] = useState<CreateGuildStep | null>(null);
-
-  const handleChangeModal = (modal: React.ReactNode) => {
-    openModal('basic', modal);
-  };
-
-  useEffect(() => {
-    if (!modal.basic) {
-      setCurrentModal(null);
-    }
-  }, [modal]);
-
-  useEffect(() => {
-    if (currentModal === null) return;
-
-    switch (currentModal) {
-      case 'initial':
-        handleChangeModal(<CreateGuildModal setCurrentModal={setCurrentModal} />);
-        break;
-      case 'customize':
-        handleChangeModal(<CustomizeGuildModal setCurrentModal={setCurrentModal} />);
-        break;
-    }
-  }, [currentModal]);
-
   return (
     <S.GuildList>
       <S.DMButton>
         <S.DiscordIcon size={32} />
       </S.DMButton>
       {/* 서버 리스트 추가 예정 */}
-      <S.AddGuildButton onClick={() => setCurrentModal('initial')}>
-        <S.PlusIcon size={24} />
-      </S.AddGuildButton>
+      <AddGuildButton />
       <S.SearchCommunityButton>
         <S.CompassIcon size={36} />
       </S.SearchCommunityButton>
