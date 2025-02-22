d490d651e97f14c9d8684db92411e575f5b8389d
zelkovaria <hyein396@gmail.com>
2025-02-22 21:54:50 +0900

    [FE] fix: FriendPage 레이아웃 배치 수정 (#66)

--- src/frontend/src/pages/FriendsPage/index.tsx
+++ src/frontend/src/pages/FriendsPage/index.tsx
@@ -1,11 +1,19 @@
+import GuildCategories from '@/components/guild/GuildCategories';
+import { useGuildInfoStore } from '@/stores/guildInfo';
+
 import GuildList from '../../components/guild/GuildList';
 
 import * as S from './styles';
 
 const FriendsPage = () => {
+  const { guildId } = useGuildInfoStore();
+
   return (
     <S.FriendsPage>
-      <GuildList />
+      <S.ContentContainer>
+        <GuildList />
+        {guildId && <GuildCategories guildId={guildId} />}
+      </S.ContentContainer>
     </S.FriendsPage>
   );
 };
