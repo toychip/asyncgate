67e5f2334c72e7738194cfe1c7405a1c14785699
chysis <chysiss@naver.com>
2025-02-22 20:49:12 +0900

    [FE] feat: 유저 관련 api 및 타입 추가 (#34)

---
+++ src/frontend/src/api/users.ts
@@ -0,0 +1,51 @@
+import { endPoint } from '@/constants/endPoint';
+import {
+  PatchUserInfoRequest,
+  PatchUserInfoResponse,
+  PostAuthCodeRequest,
+  PostAuthCodeResponse,
+  PostEmailDuplicateResponse,
+  PostLoginRequest,
+  PostLoginResponse,
+  PostRegisterRequest,
+  PostRegisterResponse,
+} from '@/types/users';
+import { publicAxios, tokenAxios } from '@/utils/axios';
+
+export const postLogin = async (requestBody: PostLoginRequest) => {
+  const { data } = await publicAxios.post<PostLoginResponse>(endPoint.users.POST_SIGN_IN, requestBody);
+  return data;
+};
+
+export const postRegister = async (requestBody: PostRegisterRequest) => {
+  const { data } = await publicAxios.post<PostRegisterResponse>(endPoint.users.POST_SIGN_UP, requestBody);
+  return data;
+};
+
+export const postAuthCode = async (requestBody: PostAuthCodeRequest) => {
+  const { data } = await tokenAxios.post<PostAuthCodeResponse>(endPoint.users.POST_AUTHENTICATION_CODE, requestBody);
+  return data;
+};
+
+interface PostEmailDuplicateParams {
+  email: string;
+}
+
+export const postEmailDuplicate = async ({ email }: PostEmailDuplicateParams) => {
+  const { data } = await publicAxios.post<PostEmailDuplicateResponse>(endPoint.users.POST_VALIDATION_EMAIL, null, {
+    params: { email },
+  });
+  return data;
+};
+
+interface PatchUserInfoParams {
+  userId: string;
+  bodyRequest: PatchUserInfoRequest;
+}
+
+export const PatchUserInfo = async ({ userId, bodyRequest }: PatchUserInfoParams) => {
+  const { data } = await publicAxios.patch<PatchUserInfoResponse>(endPoint.users.PATCH_USER_INFO, bodyRequest, {
+    params: userId,
+  });
+  return data;
+};
---
+++ src/frontend/src/types/users.ts
@@ -0,0 +1,57 @@
+// result가 없는 기본 응답
+export interface UserDefaultResponse {
+  httpStatus: number;
+  message: string;
+  time: Date;
+  result: null;
+}
+
+export interface PostLoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface PostLoginResponse {
+  httpStatus: number;
+  message: string;
+  time: Date;
+  result: {
+    access_token: string;
+  };
+}
+
+export interface PostRegisterRequest {
+  email: string;
+  password: string;
+  name: string;
+  nickname: string;
+  birth: string;
+}
+
+export type PostRegisterResponse = UserDefaultResponse;
+
+export interface PostAuthCodeRequest {
+  email: string;
+  authentication_code: string;
+}
+
+export type PostAuthCodeResponse = UserDefaultResponse;
+
+export interface PostEmailDuplicateResponse {
+  httpStatus: number;
+  message: string;
+  time: Date;
+  result: {
+    is_duplicate: boolean;
+  };
+}
+
+export interface PatchUserInfoRequest {
+  name: string;
+  nickname: string;
+  profile_image: string;
+}
+
+export type PatchUserInfoResponse = UserDefaultResponse;
+
+export type DeleteUserInfoResponse = UserDefaultResponse;
