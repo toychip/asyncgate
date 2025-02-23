888cb33537875ca3d68a0e7e48485467d32979fa
zelkovaria <hyein396@gmail.com>
2025-02-23 17:32:34 +0900

    [FE] feat: 채널 생성 클릭시 모달 열림 (#63)

--- src/frontend/src/components/guild/CategoriesList/index.tsx
+++ src/frontend/src/components/guild/CategoriesList/index.tsx
@@ -1,22 +1,32 @@
 import { TbPlus } from 'react-icons/tb';
 
+import { useGuildInfoStore } from '@/stores/guildInfo';
+import useModalStore from '@/stores/modalStore';
 import { BodyMediumText, BodyRegularText } from '@/styles/Typography';
 import { CategoryDataResult, ChannelResult } from '@/types/guilds';
 
+import CreateChannelModal from '../CreateChannelModal';
+
 import * as S from './styles';
 
-interface CategoriesListProps {
+export interface CategoriesListProps {
   categories?: CategoryDataResult[];
   channels?: ChannelResult[];
 }
 const CategoriesList = ({ categories, channels }: CategoriesListProps) => {
+  const { openModal } = useModalStore();
+  const { guildId } = useGuildInfoStore();
+  const handleOpenModal = (categoryId: string, guildId: string) => {
+    openModal('withFooter', <CreateChannelModal categoryId={categoryId} guildId={guildId} />);
+  };
+
   return (
     <S.CategoriesList>
       {categories?.map((category) => (
         <S.Category key={category.categoryId}>
           <S.CategoryName>
             <BodyMediumText>{category.name}</BodyMediumText>
-            <TbPlus size={18} />
+            <TbPlus size={18} onClick={() => handleOpenModal(category.categoryId, guildId)} />
           </S.CategoryName>
           {channels
             ?.filter((channel) => category.categoryId === channel.categoryId)
