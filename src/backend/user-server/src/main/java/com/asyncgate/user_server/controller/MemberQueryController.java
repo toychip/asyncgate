99028ec236f896e9aa6bcd5cd9293674236b5353
EunJiJung <bianbbc87@gmail.com>
2025-02-26 08:09:57 +0900

    [BE] refactor: config printer naming 변경 (#30)
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/backend/user-server/src/main/java/com/asyncgate/user_server/controller/MemberQueryController.java
+++ src/backend/user-server/src/main/java/com/asyncgate/user_server/controller/MemberQueryController.java
@@ -1,9 +1,12 @@
 package com.asyncgate.user_server.controller;
 
+import com.asyncgate.user_server.security.annotation.MemberID;
 import com.asyncgate.user_server.support.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Hidden
@@ -17,5 +20,14 @@ public class TestQueryController {
     @GetMapping("/health")
     public SuccessResponse<String> health() {
         return SuccessResponse.ok("security no need");
+    }
+
+    /**
+     * 1.7 회원 화상 간단 정보 조회
+     */
+    @Override
+    @GetMapping("/room/profile")
+    public SuccessResponse<?> readUserRoomProfile(@MemberID String userId) {
+        return SuccessResponse.ok("회원탈퇴 완료");
     }
 }
