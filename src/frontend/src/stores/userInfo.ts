887bcdd24aaab2a2eab29623d2b388038f9a2974
chysis <chysiss@naver.com>
2025-02-27 20:08:50 +0900

    [FE] feat: 로그인 한 유저 정보를 저장하는 store 작성 (#69)

---
+++ src/frontend/src/stores/userInfo.ts
@@ -0,0 +1,25 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+export interface UserInfo {
+  userId: string;
+}
+
+interface UserState {
+  userInfo: UserInfo | null;
+  setUserInfo: (user: UserInfo) => void;
+  clearUserInfo: () => void;
+}
+
+export const useChannelInfoStore = create<UserState>()(
+  persist(
+    (set) => ({
+      userInfo: null,
+      setUserInfo: (user) => set({ userInfo: user }),
+      clearUserInfo: () => set({ userInfo: null }),
+    }),
+    {
+      name: 'userInfo',
+    },
+  ),
+);
