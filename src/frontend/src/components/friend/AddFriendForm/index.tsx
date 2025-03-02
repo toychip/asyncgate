1046cc5f20fd6ff0bbcf4530424134190904b756
chysis <chysiss@naver.com>
2025-03-02 18:00:03 +0900

    [FE] feat: 친구 추가 api 연동 및 유효성 검사 로직 구현 (#89)

--- src/frontend/src/components/friend/AddFriendForm/index.tsx
+++ src/frontend/src/components/friend/AddFriendForm/index.tsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
 
+import { getFriends, postFriendRequest } from '@/api/friends';
+
+import useGetFriendsList from '../FriendsList/hooks/useGetFriendsList';
+import useGetReceivedRequests from '../PendingFriendsList/hooks/useGetReceivedRequests';
+import useGetSentRequests from '../PendingFriendsList/hooks/useGetSentRequests';
+
 import * as S from './styles';
 
 export type ResultMessageType = 'success' | 'fail';
@@ -10,17 +16,58 @@ interface ResultMessage {
 }
 
 const AddFriendForm = () => {
-  // TODO: 친구 검색 및 요청 보내기 로직 연동
-  // TODO: 요청 보내기 결과에 따라 resultMessage를 설정하고 표시
   const [inputData, setInputData] = useState('');
   const [resultMessage, setResultMessage] = useState<ResultMessage | null>(null);
+
+  const { friends } = useGetFriendsList();
+  const { sentRequests } = useGetSentRequests();
+  const { receivedRequests } = useGetReceivedRequests();
 
   const handleInputChange = (value: string) => {
     setInputData(value);
   };
 
-  const handleSendRequestButtonClick = () => {
-    console.log('친구 요청');
+  const getFriendInfoByEmail = async () => {
+    // 1. 이미 친구로 등록된 사용자인지 확인
+    if (friends && friends.some((friend) => friend.email === inputData)) {
+      setResultMessage({ type: 'fail', content: '이미 친구로 등록된 사용자예요.' });
+      return null;
+    }
+
+    // 2. 이미 친구 요청을 보낸 사용자인지 확인
+    if (sentRequests && sentRequests.some((request) => request.email === inputData)) {
+      setResultMessage({ type: 'fail', content: '이미 친구 요청을 보냈어요. 상대방의 응답을 기다려주세요.' });
+      return null;
+    }
+
+    // 3. 상대가 이미 친구 요청을 보냈는지 확인
+    if (receivedRequests && receivedRequests.some((request) => request.email === inputData)) {
+      setResultMessage({ type: 'fail', content: '상대방이 이미 친구 요청을 보냈어요. 친구 요청을 확인해주세요.' });
+      return null;
+    }
+
+    try {
+      const response = await getFriends({ email: inputData });
+      return response;
+    } catch (error) {
+      console.log('회원 검색 실패', error);
+      setResultMessage({ type: 'fail', content: '회원 검색에 실패했어요. 다시 시도해주세요.' });
+      return null;
+    }
+  };
+
+  const handleSendRequestButtonClick = async () => {
+    try {
+      const friendInfo = await getFriendInfoByEmail();
+      if (!friendInfo) return;
+
+      await postFriendRequest({ toUserId: friendInfo.userId }); // friendInfo에 userId가 있어야 함
+
+      setResultMessage({ type: 'success', content: '친구 요청을 보냈어요!' });
+    } catch (error) {
+      console.log('친구 요청 실패', error);
+      setResultMessage({ type: 'fail', content: '친구 요청을 보내는 데 실패했어요. 다시 시도해주세요.' });
+    }
   };
 
   return (
