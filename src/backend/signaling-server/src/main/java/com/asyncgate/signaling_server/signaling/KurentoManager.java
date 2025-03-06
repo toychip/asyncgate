6215b412aa1ecfb373bb35e71b5fd9718669745d
EunJiJung <bianbbc87@gmail.com>
2025-03-06 21:10:54 +0900

    [BE] refactor: 내 answer만 수신하도록 변경 (#98)
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/backend/signaling-server/src/main/java/com/asyncgate/signaling_server/signaling/KurentoManager.java
+++ src/backend/signaling-server/src/main/java/com/asyncgate/signaling_server/signaling/KurentoManager.java
@@ -152,7 +152,7 @@ public class KurentoManager {
         System.out.println("sdp 처리 및 sdp answer 생성" + sdpAnswer);
 
         // 클라이언트에게 SDP Answer 전송
-        messagingTemplate.convertAndSend("/topic/answer/" + message.data().roomId(),
+        messagingTemplate.convertAndSend("/topic/answer/" + message.data().roomId() + userId,
                 new KurentoAnswerResponse("sdpAnswer", sdpAnswer));
     }
 
