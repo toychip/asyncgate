228fb1f26f17cab0b7dc588b0c6dd16dcbc4e73b
toychip <dlawnsgud427@naver.com>
2025-03-09 21:23:15 +0900

    [BE] fix: Persistable 로직 제거, 수정시간을 생성시간으로 변경하여 해결 (#124)

--- src/backend/chat-server/build.gradle.kts
+++ src/backend/chat-server/build.gradle.kts
@@ -33,7 +33,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     implementation("org.springframework.cloud:spring-cloud-starter")
